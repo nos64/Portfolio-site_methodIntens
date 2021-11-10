f840394814f05c6d223c29848a8febd7f82eeb26
nos64 <manosov1984@gmail.com>
2021-11-10 17:20:34 +0400

    Day-2
    Добавили закрытие модального окна при клике в свободной области страницы, Запретили скрол страницы при открытом модальном окне

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
+const disableScroll = () => {
+  const widthScroll = window.innerWidth - document.body.offsetWidth;
+  document.body.scrollPosition = window.scrollY;
+
+  document.body.style.cssText = `
+  overflow: hidden;
+  position: fixed;
+  top: -${document.body.scrollPosition}px;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
+  padding-right: ${widthScroll}px;
+`;
+};
+
+const enableScroll = () => {
+  document.body.style.cssText = `position: relative`;
+  window.scroll({ top: document.body.scrollPosition });
+};
+
 const presentOrderBtn = document.querySelector(".present__order-btn");
 const pageOverlayModal = document.querySelector(".page__overlay_modal");
 const modalClose = document.querySelector(".modal__close");
@@ -17,20 +37,21 @@ const handlerModal = (
     fast: 0.1,
   };
 
-  openBtn.addEventListener("click", () => {
+  const openModal = () => {
+    disableScroll();
     modal.style.opacity = opacity;
 
     modal.classList.add(openSelector);
-
     const anim = () => {
       opacity += speed[speedKey];
       modal.style.opacity = opacity;
       if (opacity < 1) requestAnimationFrame(anim);
     };
     requestAnimationFrame(anim);
-  });
+  };
 
-  closeTrigger.addEventListener("click", () => {
+  const closeModal = () => {
+    disableScroll();
     const anim = () => {
       opacity -= speed[speedKey];
       modal.style.opacity = opacity;
@@ -41,6 +62,16 @@ const handlerModal = (
       }
     };
     requestAnimationFrame(anim);
+  };
+
+  openBtn.addEventListener("click", openModal);
+
+  closeTrigger.addEventListener("click", closeModal);
+
+  modal.addEventListener("click", (event) => {
+    if (event.target === modal) {
+      closeModal();
+    }
   });
 };
 
