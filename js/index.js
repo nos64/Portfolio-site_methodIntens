7b3ac65f012d6083f3a3d8e09e1692777a35d2bf
nos64 <manosov1984@gmail.com>
2021-11-12 16:21:48 +0400

    Final
    Добавлен вывод портфолио по клику

--- js/index.js
+++ js/index.js
@@ -155,3 +155,81 @@ const enableScroll = () => {
     enableScroll();
   });
 }
+
+// Работа с сервером
+{
+  const COUNT_CARD = 2;
+  const portfolioList = document.querySelector(".portfolio__list");
+  const portfolioAdd = document.querySelector(".portfolio__add");
+
+  const getData = () =>
+    fetch("db.json")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw `Что-то пошло не так попробуйте позже, ошибка: ${response.status}`;
+        }
+      })
+      .catch((error) => console.error(error));
+
+  const createStore = async () => {
+    const data = await getData();
+
+    return {
+      data,
+      counter: 0,
+      count: COUNT_CARD,
+      get length() {
+        return this.data.length;
+      },
+      get cardData() {
+        const renderData = this.data.slice(
+          this.counter,
+          this.counter + this.count
+        );
+        this.counter += renderData.length;
+        return renderData;
+      },
+    };
+  };
+  // Получает данные из массива store, cardData, перебирает и выводит карточки на страницу
+  const renderCard = (data) => {
+    const cards = data.map(({ preview, year, type, client, image }) => {
+      const li = document.createElement("li");
+      li.classList.add("portfolio__item");
+      li.innerHTML = `
+      <article class="card" tabindex="0" role="button" aria-label="открыть макет" data-full-image="${image}">
+              <picture class="card__picture">
+                <source srcset="${preview}.avif" type="image/avif">
+                <source srcset="${preview}.webp" type="image/webp">
+                <img src="${preview}.jpg" alt="превью game over" width="166" height="103">
+              </picture>
+
+              <p class="card__data">
+                <span class="card__client">Клиент: ${client}</span>
+                <time class="card__date" datetime="${year}">год: ${year}</time>
+              </p>
+
+              <h3 class="card__title">${type}</h3>
+            </article>
+      `;
+      return li;
+    });
+    portfolioList.append(...cards);
+  };
+
+  // Выводит картинки из db по клику на кнопку стрелки
+  const initPortfolio = async () => {
+    const store = await createStore();
+    renderCard(store.cardData);
+    portfolioAdd.addEventListener("click", () => {
+      renderCard(store.cardData);
+      if (store.length === store.counter) {
+        portfolioAdd.remove();
+      }
+    });
+  };
+
+  initPortfolio();
+}
